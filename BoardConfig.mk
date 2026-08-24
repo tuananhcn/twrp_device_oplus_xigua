26a0a988a04ed6139c26a7aeb33e0ac29f4243f1
tuananhcn <nguyenanhtuan225tn@gmailc.om>
2026-08-24 23:57:36 +0700

    Test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -271,8 +271,8 @@ AB_OTA_PARTITIONS += \
     my_stock
 
 # Bootloader
-PRODUCT_PLATFORM                := sun
-TARGET_BOOTLOADER_BOARD_NAME    := sun
+PRODUCT_PLATFORM                := kalama
+TARGET_BOOTLOADER_BOARD_NAME    := kalama
 
 # Crypto
 BOARD_USES_METADATA_PARTITION   := true
@@ -302,13 +302,12 @@ BOARD_RAMDISK_USE_LZ4       := true
 
 # Partitions
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED  := true
-BOARD_RECOVERYIMAGE_PARTITION_SIZE      := 0x6400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE      := 104857600
 
-BOARD_SUPER_PARTITION_SIZE                  := 15569256448
-BOARD_SUPER_PARTITION_GROUPS                := qti_dynamic_partitions
-BOARD_QTI_DYNAMIC_PARTITIONS_SIZE           := 15565062144
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm odm
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST += my_bigball my_carrier my_company my_engineering my_heytap my_manifest my_preload my_product my_region my_stock
+BOARD_SUPER_PARTITION_SIZE                  := 9126805504
+BOARD_SUPER_PARTITION_GROUPS                := oneplus_dynamic_partitions
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_PARTITION_LIST := system system system_dlkm system_dlkm system_ext system_ext product product vendor vendor vendor_dlkm vendor_dlkm odm odm my_product my_product my_company my_company my_carrier my_carrier my_region my_region my_bigball my_bigball my_heytap my_heytap my_stock my_stock my_preload my_preload my_manifest my_manifest my_engineering my_engineering my_colorospro my_colorospro
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_ODM             := odm
