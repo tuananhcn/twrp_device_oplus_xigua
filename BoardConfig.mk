5b0367a81fff7376ce7a03401bd27a6a9343bed6
tuananhcn <nguyenanhtuan225tn@gmailc.om>
2026-08-28 00:58:03 +0700

    feat: enable FBE crypto support in BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ TARGET_BOOTLOADER_BOARD_NAME    := kalama
 # Crypto
 BOARD_USES_METADATA_PARTITION   := true
 TW_INCLUDE_CRYPTO               := true
-# TW_INCLUDE_CRYPTO_FBE           := true
+TW_INCLUDE_CRYPTO_FBE           := true
 # TW_INCLUDE_FBE_METADATA_DECRYPT := true
 # TW_INCLUDE_OMAPI                := true
 
