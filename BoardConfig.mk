311c3f2ccaa01e74785d6b71740ebf0825814ec1
tuananhcn <nguyenanhtuan225tn@gmailc.om>
2026-08-29 10:08:08 +0700

    refactor: remove redundant vendor recovery root files and update build configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,138 +6,123 @@
 # #
 
 # # Init
-TARGET_INIT_VENDOR_LIB          := //$(DEVICE_PATH):libinit_oplus_xigua
-TARGET_RECOVERY_DEVICE_MODULES  := libinit_oplus_xigua
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_oplus_xigua
+TARGET_RECOVERY_DEVICE_MODULES := libinit_oplus_xigua
 
 # Building with minimal manifest
-ALLOW_MISSING_DEPENDENCIES                      := true
-BUILD_BROKEN_DUP_RULES                          := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES    := true
+ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
-BUILD_BROKEN_NINJA_USES_ENV_VARS    += RTIC_MPGEN
-BUILD_BROKEN_PLUGIN_VALIDATION      := soong-libaosprecovery_defaults soong-libguitwrp_defaults soong-libminuitwrp_defaults soong-vold_defaults
+# BUILD_BROKEN_NINJA_USES_ENV_VARS    += RTIC_MPGEN
+# BUILD_BROKEN_PLUGIN_VALIDATION      := soong-libaosprecovery_defaults soong-libguitwrp_defaults soong-libminuitwrp_defaults soong-vold_defaults
 
 # Architecture
-TARGET_ARCH                 := arm64
-TARGET_ARCH_VARIANT         := armv8-2a-dotprod
-TARGET_CPU_ABI              := arm64-v8a
-TARGET_CPU_VARIANT          := generic
-TARGET_CPU_VARIANT_RUNTIME  := kryo300
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo300
 
 # A/B
-AB_OTA_PARTITIONS := \
-    boot \
-    init_boot \
-    vendor_boot \
-    dtbo \
-    odm \
-    product \
-    system \
-    system_ext \
-    system_dlkm \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
-    vendor \
-    vendor_dlkm
-
-# AB partitions for oplus
+AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
-    my_bigball \
-    my_carrier \
-    my_company \
-    my_engineering \
-    my_heytap \
-    my_manifest \
-    my_preload \
-    my_product \
-    my_region \
-    my_stock
+                     system \
+                     vendor \
+                     product \
+                     odm \
+                     system_ext
 
 # Bootloader
-PRODUCT_PLATFORM                := kalama
-TARGET_BOOTLOADER_BOARD_NAME    := kalama
+PRODUCT_PLATFORM := kalama
+TARGET_BOOTLOADER_BOARD_NAME := kalama
+TARGET_NO_BOOTLOADER := true
 
 # Crypto
-BOARD_USES_METADATA_PARTITION   := true
-TW_INCLUDE_CRYPTO               := true
-TW_INCLUDE_CRYPTO_FBE           := true
-# TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_METADATA_PARTITION := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
 # TW_INCLUDE_OMAPI                := true
 
 # Debug
-TARGET_USES_LOGD                := true
-TWRP_INCLUDE_LOGCAT             := true
-TARGET_RECOVERY_DEVICE_MODULES  += debuggerd
-TARGET_RECOVERY_DEVICE_MODULES  += strace
-RECOVERY_BINARY_SOURCE_FILES    += $(TARGET_OUT_EXECUTABLES)/debuggerd
-RECOVERY_BINARY_SOURCE_FILES    += $(TARGET_OUT_EXECUTABLES)/strace
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+TARGET_RECOVERY_DEVICE_MODULES += debuggerd
+TARGET_RECOVERY_DEVICE_MODULES += strace
+RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/debuggerd
+RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/strace
 
 # File systems
 TARGET_USERIMAGES_USE_F2FS := true
-TW_USE_DMCTL               := true
+TW_USE_DMCTL := true
 
 # Kernel & GKI Header Configuration
-BOARD_KERNEL_IMAGE_NAME     := Image
-BOARD_BOOT_HEADER_VERSION   := 4
-BOARD_KERNEL_PAGESIZE       := 4096
-BOARD_MKBOOTIMG_ARGS        += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS        += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_RAMDISK_USE_LZ4       := true
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
+BOARD_RAMDISK_USE_LZ4 := true
 
 # Partitions
-BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED         := true
-BOARD_RECOVERYIMAGE_PARTITION_SIZE          := 104857600
-BOARD_SUPER_PARTITION_SIZE                  := 16106127360
-BOARD_SUPER_PARTITION_GROUPS                := qti_dynamic_partitions
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm odm
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST += my_bigball my_carrier my_company my_engineering my_heytap my_manifest my_preload my_product my_region my_stock
-BOARD_QTI_DYNAMIC_PARTITIONS_SIZE           := 16101933056 # TODO: Fix hardcoded value
-BOARD_ODMIMAGE_FILE_SYSTEM_TYPE             := ext4
-TARGET_COPY_OUT_ODM                         := odm
-TARGET_COPY_OUT_VENDOR                      := vendor
+# BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED      := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 104857600
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_SUPER_PARTITION_SIZE := 16106127360
+BOARD_SUPER_PARTITION_GROUPS := oplus_dynamic_partitions
+BOARD_OPLUS_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor odm my_product my_engineering my_company my_carrier my_region my_heytap my_stock my_preload my_manifest
+BOARD_OPLUS_DYNAMIC_PARTITIONS_SIZE := 16101933056
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_ODM := odm
+TARGET_COPY_OUT_VENDOR := vendor
 
 # Platform
-TARGET_BOARD_PLATFORM   := sm8550
-QCOM_BOARD_PLATFORMS    += sm8550
+TARGET_BOARD_PLATFORM := sm8550
+QCOM_BOARD_PLATFORMS += sm8550
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
-BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE    := true
-TARGET_RECOVERY_PIXEL_FORMAT                := RGBX_8888
-TW_INCLUDE_FASTBOOTD                        := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TW_INCLUDE_FASTBOOTD := true
 
 # Tool
 TW_ENABLE_ALL_PARTITION_TOOLS := true
-TW_INCLUDE_7ZA                := true
-TW_INCLUDE_REPACKTOOLS        := true
-TW_INCLUDE_RESETPROP          := true
-TW_USE_TOOLBOX                := true
-TW_INCLUDE_ZSTD               := true
+TW_INCLUDE_7ZA := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_RESETPROP := true
+TW_USE_TOOLBOX := true
+TW_INCLUDE_ZSTD := true
 
 # TWRP display
-TW_BRIGHTNESS_PATH      := /sys/class/backlight/panel0-backlight/brightness
-TW_DEFAULT_BRIGHTNESS   := 1000
-TW_FRAMERATE            := 60
-TW_MAX_BRIGHTNESS       := 2047
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel0-backlight/brightness
+TW_DEFAULT_BRIGHTNESS := 1000
+TW_FRAMERATE := 60
+TW_MAX_BRIGHTNESS := 2047
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_THEME                := portrait_hdpi
+TW_THEME := portrait_hdpi
 
 # TWRP file system
-RECOVERY_SDCARD_ON_DATA     := true
-TARGET_USES_MKE2FS          := true
-TW_ENABLE_FS_COMPRESSION    := true
-TW_INCLUDE_FUSE_EXFAT       := true
-TW_INCLUDE_FUSE_NTFS        := true
-TW_INCLUDE_NTFS_3G          := true
-TW_NO_EXFAT_FUSE            := true
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_USES_MKE2FS := true
+TW_ENABLE_FS_COMPRESSION := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_INCLUDE_FUSE_NTFS := true
+TW_INCLUDE_NTFS_3G := true
+TW_NO_EXFAT_FUSE := true
 
 # Version
-PLATFORM_VERSION                := 99.87.36
-PLATFORM_VERSION_LAST_STABLE    := $(PLATFORM_VERSION)
-PLATFORM_SECURITY_PATCH         := 2099-12-31
-VENDOR_SECURITY_PATCH           := $(PLATFORM_SECURITY_PATCH)
-TW_DEVICE_VERSION               := OPLUS-ACE2PRO-XIGUA
+PLATFORM_VERSION := 99.87.36
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+TW_DEVICE_VERSION := OPLUS-ACE2PRO-XIGUA
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
@@ -146,17 +131,18 @@ BOARD_AVB_ENABLE := true
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 
 # Other TWRP Configurations
-TARGET_RECOVERY_QCOM_RTC_FIX            := true
-TW_CUSTOM_CPU_TEMP_PATH                 := "/sys/class/thermal/thermal_zone75/temp"
-TW_POWER_SUPPLY_BATTERY_PATH            := "/sys/class/power_supply/battery"
-TW_USE_BATTERY_SYSFS_STATS              := true
-TW_BATTERY_SYSFS_WAIT_SECONDS           := 8
-TW_USE_LEGACY_BATTERY_SERVICES          := true 
-TW_EXCLUDE_APEX                         := true
-TW_EXCLUDE_DEFAULT_USB_INIT             := true
-TW_EXTRA_LANGUAGES                      := true
-TW_LOAD_VENDOR_MODULES                  := "adsp_loader_dlkm.ko oplus_chg_v2.ko stm_st54se_gpio.ko nxp-nci.ko"
-TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI      := true
-TW_NO_SCREEN_BLANK                      := true
-TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID  := true
-TW_NO_NETWORK                           := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone75/temp"
+TW_POWER_SUPPLY_BATTERY_PATH := "/sys/class/power_supply/battery"
+TW_USE_BATTERY_SYSFS_STATS := true
+TW_BATTERY_SYSFS_WAIT_SECONDS := 8
+TW_USE_LEGACY_BATTERY_SERVICES := true
+TW_EXCLUDE_APEX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko oplus_chg_v2.ko stm_st54se_gpio.ko nxp-nci.ko"
+TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
+TW_NO_SCREEN_BLANK := true
+TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
+TW_NO_NETWORK := true
+TW_OZIP_DECRYPT_KEY := "0"
